aa96f36a7da0b62c662dfa71a086573da321422b
Adesh Sookwa <ksookwa@outlook.com>
2023-03-26 14:04:00 -0400

    Commit 2 up Barebone

--- UPS/App.tsx
+++ UPS/App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwind-rn/dist';
+import CustomersScreen from './screens/CustomersScreen';
+import  utilities  from './tailwind.json'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+
+////////////////    Imports End /////////////////
+
+
+//////////////////////////////////////////////////////////
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    //@ts-ignore -TailwindProvider is missing a type definition
+    <TailwindProvider utilities={utilities}>
+      <NavigationContainer>
+        <CustomersScreen />
+      </NavigationContainer>
+      
+    </TailwindProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
